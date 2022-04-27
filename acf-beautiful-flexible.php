6d2f99e09ec3d1c68815ef340ffb0aad109679b2
Maxime CULEA <MaximeCulea@users.noreply.github.com>
2022-04-27 22:17:13 +0200

    Update plugin header

--- acf-beautiful-flexible.php
+++ acf-beautiful-flexible.php
@@ -1,33 +1,42 @@
 <?php
-/*
- Plugin Name: ACF Beautiful Flexible
- Version: 1.0.1
- Plugin URI: https://wordpress.org/plugins/acf-beautiful-flexible
- Description: Transform ACF's flexible layouts list into a beautiful popup.
- Author: Maxime Culea
- Author URI: https://profiles.wordpress.org/MaximeCulea
- Domain Path: languages
- Text Domain: acf-beautiful-flexible
- Contributors: Maxime Culea
-
- ----
-
- Copyright 2022 Maxime CULEA (https://profiles.wordpress.org/MaximeCulea)
-
- This program is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 2 of the License, or
- (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program; if not, write to the Free Software
- Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+/**
+ * ACF Beautiful Flexible
+ *
+ * @package           ACFBeautifulFlexible
+ * @author            Maxime Culea
+ * @copyright         2022 Maxime Culea
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       ACF Beautiful Flexible
+ * Plugin URI:        https://wordpress.org/plugins/acf-beautiful-flexible
+ * Description:       Transform ACF's flexible layouts list into a beautiful popup.
+ * Version:           1.0.1
+ * Requires at least: 4.7
+ * Requires PHP:      7.4
+ * Author:            Maxime Culea
+ * Author URI:        https://profiles.wordpress.org/MaximeCulea
+ * Text Domain:       acf-beautiful-flexible
+ * License:           GPL v2 or later
+ * License URI:       https://github.com/MaximeCulea/acf-beautiful-flexible/blob/master/LICENSE.md
+ * Contributors:      maximeculea
+ * Donate link:       https://www.paypal.com/paypalme/MaximeCulea
  */
+
+/*
+ACF Beautiful Flexible is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+ 
+ACF Beautiful Flexible is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with ACF Beautiful Flexible. If not, see https://github.com/MaximeCulea/acf-beautiful-flexible/blob/master/LICENSE.md.
+*/
 
 // don't load directly
 if ( ! defined( 'ABSPATH' ) ) {
@@ -36,7 +45,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 // Plugin constants
 define( 'ACF_BEAUTIFUL_FLEXIBLE_VERSION', '1.0.1' );
-define( 'ACF_BEAUTIFUL_FLEXIBLE_MIN_PHP_VERSION', '5.6' );
+define( 'ACF_BEAUTIFUL_FLEXIBLE_MIN_PHP_VERSION', '7.4' );
 
 // Plugin URL and PATH
 define( 'ACF_BEAUTIFUL_FLEXIBLE_URL', plugin_dir_url( __FILE__ ) );
@@ -54,11 +63,11 @@ if ( version_compare( PHP_VERSION, ACF_BEAUTIFUL_FLEXIBLE_MIN_PHP_VERSION, '<' )
 	return;
 }
 
-// Autoload all the things \o/
+// Autoload
 require_once ACF_BEAUTIFUL_FLEXIBLE_DIR . 'autoload.php';
 
+// Init the plugin
 add_action( 'plugins_loaded', 'plugins_loaded_acf_beautiful_flexible_plugin' );
-/** Init the plugin */
 function plugins_loaded_acf_beautiful_flexible_plugin() {
 	$requirements = ACF_Beautiful_Flexible\Requirements::get_instance();
 	if ( ! $requirements->check_requirements() ) {
