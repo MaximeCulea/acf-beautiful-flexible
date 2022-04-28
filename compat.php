f101f71217aaf980e15713504c1953d066d42e07
Maxime CULEA <MaximeCulea@users.noreply.github.com>
2022-04-28 13:03:37 +0200

    PHPCS

--- compat.php
+++ compat.php
@@ -21,7 +21,7 @@ class Compatibility {
 		load_plugin_textdomain( 'acf-beautiful-flexible', false, ACF_BEAUTIFUL_FLEXIBLE_PLUGIN_DIRNAME . '/languages' );
 
 		 /* translators: %s: Minimal PHP Version */
-		trigger_error( sprintf( esc_html__( 'ACF Beautiful Flexible requires PHP version %s or greater to be activated.', 'acf-beautiful-flexible' ), ACF_BEAUTIFUL_FLEXIBLE_MIN_PHP_VERSION ) );
+		trigger_error( sprintf( esc_html__( 'ACF Beautiful Flexible requires PHP version %s or greater to be activated.', 'acf-beautiful-flexible' ), ACF_BEAUTIFUL_FLEXIBLE_MIN_PHP_VERSION ) ); // phpcs:disable WordPress.Security.EscapeOutput.OutputNotEscaped
 
 		// Deactive self
 		deactivate_plugins( ACF_BEAUTIFUL_FLEXIBLE_DIR . 'acf-beautiful-flexible.php' );
@@ -38,7 +38,7 @@ class Compatibility {
 		echo '<div class="notice error is-dismissible">';
 
 		 /* translators: %1$s: Minimal PHP Version required, %2$s: Current PHP Version */
-		echo '<p>' . sprintf( esc_html__( 'ACF Beautiful Flexible require PHP version %1$s or greater to be activated. Your server is currently running PHP version %2$s.', 'acf-beautiful-flexible' ), ACF_BEAUTIFUL_FLEXIBLE_MIN_PHP_VERSION, PHP_VERSION ) . '</p>';
+		echo '<p>' . sprintf( esc_html__( 'ACF Beautiful Flexible require PHP version %1$s or greater to be activated. Your server is currently running PHP version %2$s.', 'acf-beautiful-flexible' ), ACF_BEAUTIFUL_FLEXIBLE_MIN_PHP_VERSION, PHP_VERSION ) . '</p>'; // phpcs:disable WordPress.Security.EscapeOutput.OutputNotEscaped
 		echo '</div>';
 	}
 }
